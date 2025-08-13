42c44343e85f919a0357066e0177027db5303991
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-08-13 23:23:23 +0200

    Create NavBar component with lucide icons

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,27 @@
+import { ROUTES } from '@/lib/routes';
+import { BookType, Heart, Info } from 'lucide-react';
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <nav className="mb-4 py-4">
+      <ul className="flex justify-around">
+        <li>
+          <Link href={ROUTES.USER_GUIDE}>
+            <Info size={32} />
+          </Link>
+        </li>
+        <li>
+          <Link href={ROUTES.WORD_LIST}>
+            <BookType size={32} />
+          </Link>
+        </li>
+        <li>
+          <Link href={ROUTES.HEALTH_CHECK}>
+            <Heart size={32} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
