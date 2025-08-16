bd00451388b1ddd40d38d2e3934b696ef3d19f13
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-08-16 18:13:20 +0200

    Split WordListTable into client-side component with sorting

--- src/app/word-list/page.tsx
+++ src/app/word-list/page.tsx
@@ -1,14 +1,13 @@
-import { wordListTable } from '@/db/schema';
-import { getWordList } from '@/lib/actions';
+import { WordListTable } from '@/components/WordListTable';
 import { ROUTES } from '@/lib/routes';
-import { getTableColumns } from 'drizzle-orm';
+import { getWordList } from '@/lib/wordListActions';
 import { House, Plus } from 'lucide-react';
 import Link from 'next/link';
 
 export default async function WordListPage() {
   const wordList = await getWordList();
-  const columns = getTableColumns(wordListTable);
-  const colNames = Object.keys(columns).filter((key) => key !== 'id');
+  // const columns = getTableColumns(wordListTable);
+  // const colNames = Object.keys(columns).filter((key) => key !== 'id');
 
   return (
     <main className="content-grid grid-rows-[auto_1fr] justify-items-center items-start">
@@ -23,25 +22,7 @@ export default async function WordListPage() {
           </Link>
         </nav>
       </header>
-      <table className="table-fixed text-center w-[90%] m-4 border-collapse ">
-        <thead>
-          <tr>
-            {colNames.map((name) => (
-              <th key={name} className="p-2 border border-neutral-300">
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className="[&>tr:nth-child(odd)]:bg-white [&>tr:nth-child(even)]:bg-neutral-100 ">
-          {wordList.map(({ id, swedish, english }) => (
-            <tr key={id}>
-              <td className="border border-neutral-300">{swedish}</td>
-              <td className="border border-neutral-300">{english}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <WordListTable wordList={wordList} />
     </main>
   );
 }
