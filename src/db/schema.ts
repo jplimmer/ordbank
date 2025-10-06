8033dad26e949d2520a3556898b42f22d32185af
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-06 17:56:41 +0200

    Update schema to use pgTable and add language pairs table with relations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,54 @@
-import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
-import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { InferInsertModel, InferSelectModel, relations } from 'drizzle-orm';
+import { integer, pgTable, text, varchar } from 'drizzle-orm/pg-core';
 
-export const users = sqliteTable('users', {
-  id: int().primaryKey({ autoIncrement: true }),
+// Tables
+export const users = pgTable('users', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const languagePairs = pgTable('language_pairs', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: integer()
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  source: text().notNull(),
+  target: text().notNull(),
   name: text().notNull(),
 });
 
-export const vocabulary = sqliteTable('vocabulary', {
-  id: int('id').primaryKey({ autoIncrement: true }),
-  source: text().unique().notNull(),
+export const vocabulary = pgTable('vocabulary', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  languagePairId: integer()
+    .notNull()
+    .references(() => languagePairs.id, { onDelete: 'cascade' }),
+  source: text().notNull().unique(),
   target: text().notNull(),
 });
 
+// Relationships
+export const userRelations = relations(users, ({ many }) => ({
+  languagePairs: many(languagePairs),
+}));
+
+export const languagePairsRelations = relations(
+  languagePairs,
+  ({ one, many }) => ({
+    user: one(users, {
+      fields: [languagePairs.userId],
+      references: [users.id],
+    }),
+    vocabulary: many(vocabulary),
+  })
+);
+
+export const vocabularyRelations = relations(vocabulary, ({ one }) => ({
+  languagePair: one(languagePairs, {
+    fields: [vocabulary.languagePairId],
+    references: [languagePairs.id],
+  }),
+}));
+
+// Generated types
 export type VocabItem = InferSelectModel<typeof vocabulary>;
 export type NewVocabItem = InferInsertModel<typeof vocabulary>;
