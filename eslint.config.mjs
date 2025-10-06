4df97c1252dac2b0eae240c8de2ae60ab40144d4
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-06 15:15:55 +0200

    Use latest eslint-config-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,19 @@
-// import { FlatCompat } from '@eslint/eslintrc';
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends(
-//     'next/core-web-vitals',
-//     'next/typescript',
-//     'plugin:prettier/recommended'
-//   ),
-// ];
-
-// export default eslintConfig;
-
+import next from 'eslint-config-next';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
-import nextConfig from 'next/eslint-config-next';
 
 export default [
-  nextConfig,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
+  next(),
   prettierConfig,
   {
     plugins: {
