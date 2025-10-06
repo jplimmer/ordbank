c1b240be750b8eec5e5acbab66b0a4462f3ea7d7
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-06 14:47:19 +0200

    Convert eslint setup to flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,37 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+// import { FlatCompat } from '@eslint/eslintrc';
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends(
-    'next/core-web-vitals',
-    'next/typescript',
-    'plugin:prettier/recommended'
-  ),
+// const eslintConfig = [
+//   ...compat.extends(
+//     'next/core-web-vitals',
+//     'next/typescript',
+//     'plugin:prettier/recommended'
+//   ),
+// ];
+
+// export default eslintConfig;
+
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import nextConfig from 'next/eslint-config-next';
+
+export default [
+  nextConfig,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ];
-
-export default eslintConfig;
