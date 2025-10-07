59c2cf1bbc2e7c86a63f7f1c8f31599029c0f718
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-07 22:28:38 +0200

    Removed old vocab table files

--- src/components/VocabPage/types.ts
+++
@@ -1,5 +0,0 @@
-import { VocabItem } from '@/lib/db/schema';
-
-export type VocabTableKeys = Exclude<keyof VocabItem, 'id'>;
-export type SortDirection = 'asc' | 'desc';
-export type EditForm = Record<VocabTableKeys, string>;
