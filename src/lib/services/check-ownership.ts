a17ab43669ebca156ff902d7ae6fc9e6c0efd527
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-08 20:56:00 +0200

    Add helper function to check vocab item belongs to language pair

--- src/lib/services/check-ownership.ts
+++ src/lib/services/check-ownership.ts
@@ -1,6 +1,6 @@
 import { and, eq } from 'drizzle-orm';
 import { db } from '../db';
-import { languagePairs } from '../db/schema';
+import { languagePairs, vocabulary } from '../db/schema';
 
 export const languagePairBelongsToUser = async (
   userId: number,
@@ -25,6 +25,36 @@ export const assertLanguagePairOwnership = async (
   if (!belongs) {
     throw new Error(
       'Unauthorised: Language pair does not belong to the current user.'
+    );
+  }
+};
+
+export const vocabItemBelongsToLanguagePair = async (
+  languagePairId: number,
+  vocabItemId: number
+): Promise<boolean> => {
+  const result = await db.query.vocabulary.findFirst({
+    where: and(
+      eq(vocabulary.id, vocabItemId),
+      eq(vocabulary.languagePairId, languagePairId)
+    ),
+    columns: { id: true },
+  });
+
+  return result !== undefined;
+};
+
+export const assertVocabItemOwnership = async (
+  languagePairId: number,
+  vocabItemId: number
+) => {
+  const belongs = await vocabItemBelongsToLanguagePair(
+    languagePairId,
+    vocabItemId
+  );
+  if (!belongs) {
+    throw new Error(
+      'Error: Vocab item does not belong to the specified lanugage pair.'
     );
   }
 };
