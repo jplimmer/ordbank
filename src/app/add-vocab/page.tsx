bea7a4289aa283d88cee727de8e804850de51d87
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-09 22:20:28 +0200

    Remove redundant add vocab routes and components

--- src/app/add-vocab/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AddVocabHandler } from '@/components/AddVocab/AddVocabHandler';
-
-export default function AddWordPage() {
-  return (
-    <div className="flex justify-center items-center p-6">
-      <AddVocabHandler />
-    </div>
-  );
-}
