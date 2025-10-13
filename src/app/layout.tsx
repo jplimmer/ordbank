0185b0b0e0655c06f0a70cea19907f8f31dd1997
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-13 16:55:19 +0200

    Add new LanguagePairProvider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import { NavBar, ThemeToggle } from '@/components/layout';
+import { LanguagePairProvider } from '@/contexts/language-pair';
 import { ThemeProvider } from '@/contexts/theme-provider';
+import { getActiveLanguagePair } from '@/lib/services/active-language-pair';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { notFound } from 'next/navigation';
 import { Toaster } from 'react-hot-toast';
 import './globals.css';
 
@@ -20,13 +23,22 @@ export const metadata: Metadata = {
   description: 'A Next.js app for learning vocabulary',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
   modal,
 }: Readonly<{
   children: React.ReactNode;
   modal: React.ReactNode;
 }>) {
+  // TO DO - integrate authentication with LanguagePairContext
+  const userId = 1;
+
+  const langPairResult = await getActiveLanguagePair(userId);
+  // TO DO - redirect to account to set active language pair if none found
+  if (!langPairResult.success) {
+    notFound();
+  }
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -41,15 +53,17 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange={false}
         >
-          <div className="content-grid grid-rows-[auto_1fr_auto] min-h-svh">
-            <header className="full-width content-grid justify-items-end pt-1">
-              <ThemeToggle />
-            </header>
-            <main className="full-width content-grid">{children}</main>
-            <footer>
-              <NavBar />
-            </footer>
-          </div>
+          <LanguagePairProvider initialPair={langPairResult.data}>
+            <div className="content-grid grid-rows-[auto_1fr_auto] min-h-svh">
+              <header className="full-width content-grid justify-items-end pt-1">
+                <ThemeToggle />
+              </header>
+              <main className="full-width content-grid">{children}</main>
+              <footer>
+                <NavBar />
+              </footer>
+            </div>
+          </LanguagePairProvider>
           {modal}
           <Toaster />
         </ThemeProvider>
