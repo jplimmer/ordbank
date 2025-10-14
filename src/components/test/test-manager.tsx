604b83ea608ba2cf34a32ddba2bebf481c7f9ed8
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-14 18:36:10 +0200

    Create temporary TestManager to display question data

---
+++ src/components/test/test-manager.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { getQuestion } from '@/lib/actions/test';
+import { Question, TestSettings } from '@/lib/types/test';
+import { useState } from 'react';
+import { Button } from '../ui/button';
+
+export function TestManager({ settings }: { settings: TestSettings }) {
+  const [question, setQuestion] = useState<Question | undefined>(undefined);
+
+  return (
+    <div>
+      <Button
+        onClick={async () => {
+          const q = await getQuestion(settings.direction, settings.answerMode);
+          setQuestion(q);
+        }}
+      >
+        Question
+      </Button>
+      <div>
+        {question &&
+          Object.entries(question).map(([key, value]) => (
+            <p key={key}>
+              <strong>{key}:</strong>{' '}
+              {Array.isArray(value) ? value.join(', ') : String(value)}
+            </p>
+          ))}
+      </div>
+    </div>
+  );
+}
