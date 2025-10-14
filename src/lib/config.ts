227064dc67ece9d17220daa614d00db6b0e8b360
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-14 09:11:28 +0200

    Remove redundant old language config

--- src/lib/config.ts
+++
@@ -1,8 +0,0 @@
-import { LanguageCode } from './languages';
-
-export const APP_CONFIG = {
-  defaultLanguages: {
-    source: 'sv' as LanguageCode,
-    target: 'en' as LanguageCode,
-  },
-} as const;
