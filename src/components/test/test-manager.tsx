d1e826f5605c2c503d7dc29b842de9a81dfd5ea9
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-15 11:52:39 +0200

    Move initial question fetching into test page

--- src/components/test/test-manager.tsx
+++ src/components/test/test-manager.tsx
@@ -1,43 +1,34 @@
 'use client';
 
-import { getQuestion } from '@/lib/actions/test';
 import { Question, TestSettings } from '@/lib/types/test';
 import { useState } from 'react';
-import { Button } from '../ui/button';
 import { MultipleChoiceAnswer } from './multiple-choice-answer';
 import { QuestionPanel } from './question-panel';
 import { TypedAnswer } from './typed-answer';
 
-export function TestManager({ settings }: { settings: TestSettings }) {
-  const [question, setQuestion] = useState<Question | undefined>(undefined);
+interface TestManagerProps {
+  settings: TestSettings;
+  initialQuestion: Question;
+}
+
+export function TestManager({ settings, initialQuestion }: TestManagerProps) {
+  const [question, setQuestion] = useState<Question>(initialQuestion);
   const [currentAnswer, setCurrentAnswer] = useState<string>('');
 
   return (
-    <div className="space-y-8">
-      <Button
-        onClick={async () => {
-          const q = await getQuestion(settings.direction, settings.answerMode);
-          setQuestion(q);
-        }}
-      >
-        Question
-      </Button>
-      {question && (
-        <div className="space-y§-4">
-          <QuestionPanel
-            questionWord={question.question}
-            direction={question.direction}
-          />
-          {question.answerMode === 'multipleChoice' ? (
-            <MultipleChoiceAnswer
-              options={question.answers}
-              value={currentAnswer}
-              setAnswer={setCurrentAnswer}
-            />
-          ) : (
-            <TypedAnswer value={currentAnswer} setAnswer={setCurrentAnswer} />
-          )}
-        </div>
+    <div className="space-y-4">
+      <QuestionPanel
+        questionWord={question.question}
+        direction={question.direction}
+      />
+      {question.answerMode === 'multipleChoice' ? (
+        <MultipleChoiceAnswer
+          options={question.answers}
+          value={currentAnswer}
+          setAnswer={setCurrentAnswer}
+        />
+      ) : (
+        <TypedAnswer value={currentAnswer} setAnswer={setCurrentAnswer} />
       )}
     </div>
   );
