5dd182952656e7f0edc094651d1c67756154b451
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-17 16:17:20 +0200

    Create saveSettings server action

--- src/lib/actions/test-settings.ts
+++ src/lib/actions/test-settings.ts
@@ -1,3 +1,48 @@
-import { UpdateTestSettings } from '../types/test';
+import { updateTestSettings } from '../services/test-settings';
+import { FormResult } from '../types/common';
+import { handleValidationError } from '../utils';
+import { testSettingsUpdateSchema } from '../validation/test-settings-schemas';
 
-export const saveSettings = async (updates: UpdateTestSettings) => {};
+export const saveSettings = async (
+  testSettingsId: number,
+  prevState: FormResult<null>,
+  formData: FormData
+): Promise<FormResult<null>> => {
+  // TO DO - Authenticate user profile with error-handling
+  const userId = 1;
+
+  // Untyped obejct from formData for validation
+  const updates = {
+    direction: formData.get('direction'),
+    answerMode: formData.get('answerMode'),
+    questionLimit: formData.get('questionLimit'),
+    timeLimitMins: formData.get('timeLimitMins'),
+  };
+
+  // Parse form data before sending to service (fail fast)
+  const parseResult = testSettingsUpdateSchema.safeParse(updates);
+  if (!parseResult.success) {
+    const validationError = handleValidationError(
+      parseResult.error,
+      'Update test settings'
+    );
+    return {
+      success: false,
+      error: validationError.message,
+      fieldErrors: validationError.fieldErrors,
+      formData,
+    };
+  }
+
+  // Update item in database
+  const updateResult = await updateTestSettings(
+    userId,
+    testSettingsId,
+    parseResult.data
+  );
+  if (!updateResult.success) {
+    return { success: false, error: updateResult.error, formData: formData };
+  }
+
+  return { success: true, data: null };
+};
