d9cd3aee186f0293c164b98dd3373b0c13768f4b
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-22 22:57:34 +0200

    Added clerk's openUserProfile to account menu

--- src/components/layout/nav-bar.tsx
+++ src/components/layout/nav-bar.tsx
@@ -1,49 +1,21 @@
 'use client';
 
 import { ROUTES } from '@/lib/constants/routes';
-import { useClerk } from '@clerk/nextjs';
-import { BookType, Home, Info, UserRound } from 'lucide-react';
+import { BookType, Home, Info } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Button } from '../ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '../ui/dropdown-menu';
+import { AccountMenu } from './account-menu';
 
 export function NavBar() {
   const pathname = usePathname();
   const isHome = pathname === ROUTES.HOME;
 
-  const { signOut } = useClerk();
-
   return (
     <nav className="pb-4">
       <ul className="flex justify-around">
         <li>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="py-2 h-fit cursor-pointer">
-                <UserRound className="size-8" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
-              <DropdownMenuItem className="text-lg">
-                <Link
-                  href={ROUTES.ACCOUNT}
-                  aria-label="Account"
-                  className="h-fit"
-                >
-                  Languages
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="text-lg" onClick={() => signOut()}>
-                Sign out
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <AccountMenu />
         </li>
         <li>
           <Button variant="ghost" className="py-2" asChild>
