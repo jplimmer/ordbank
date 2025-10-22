62816101fa143cee53539d1dd96ed051368c362a
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-22 11:59:50 +0200

    Add empty placeholder to languages table

--- src/components/languages/languages-table.tsx
+++ src/components/languages/languages-table.tsx
@@ -1,8 +1,16 @@
 'use client';
 
 import { useLanguagePairContext } from '@/contexts/language-pair';
+import { NotebookPen } from 'lucide-react';
 import { use } from 'react';
 import { DataTable } from '../ui/data-table';
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '../ui/empty';
 import { LanguagePairTableEntry, languagesColumns } from './languages-columns';
 
 interface LanguagesTableProps {
@@ -17,13 +25,28 @@ export function LanguagesTable({ dataPromise }: LanguagesTableProps) {
     <DataTable
       columns={languagesColumns}
       data={data}
+      empty={empty}
+      filter={false}
       getRowClassName={(languagePair) => {
         const isActive = languagePair.id === activePair.id;
         return isActive
           ? 'bg-indigo-100 border-l-4 border-l-indigo-300 dark:bg-indigo-900'
           : '';
       }}
-      filter={false}
     />
   );
 }
+
+const empty = (
+  <Empty>
+    <EmptyHeader>
+      <EmptyMedia variant="icon">
+        <NotebookPen />
+      </EmptyMedia>
+      <EmptyTitle>No language pairs</EmptyTitle>
+      <EmptyDescription>
+        Create a language pair to get started!
+      </EmptyDescription>
+    </EmptyHeader>
+  </Empty>
+);
