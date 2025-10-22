7aa41a394169226004af35c495dde2da749aa7f7
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-22 23:44:28 +0200

    Add manifest.json to public routes in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/user-guide']);
+const isPublicRoute = createRouteMatcher(['/user-guide', '/manifest.json']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
