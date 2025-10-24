0d5898564a9018377c6055a3c7643521f9cafca5
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-24 19:16:30 +0200

    Extract fallback from language pair guard component

--- src/components/guards/require-language-pair.tsx
+++ src/components/guards/require-language-pair.tsx
@@ -1,18 +1,7 @@
 'use client';
 
 import { useLanguagePairContext } from '@/contexts/language-pair';
-import { ROUTES } from '@/lib/constants/routes';
-import { ShieldAlert } from 'lucide-react';
-import Link from 'next/link';
-import { Button } from '../ui/button';
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemMedia,
-  ItemTitle,
-} from '../ui/item';
+import { NoLanguagePairFallback } from '../fallbacks/no-language-pair-fallback';
 
 export function RequireLanguagePair({
   children,
@@ -22,34 +11,7 @@ export function RequireLanguagePair({
   const { activePair } = useLanguagePairContext();
 
   if (!activePair) {
-    return (
-      <div className="grid justify-items-center items-center w-full h-full">
-        <Item variant="outline" className="flex-col sm:flex-row">
-          <ItemMedia
-            variant="icon"
-            className="!self-center sm:!self-start size-10 sm:size-8"
-          >
-            <ShieldAlert className="size-5 sm:size-4" />
-          </ItemMedia>
-          <ItemContent>
-            <ItemTitle>No language selected</ItemTitle>
-            <ItemDescription className="text-pretty">
-              Please select or create a language pair to continue.
-            </ItemDescription>
-          </ItemContent>
-          <ItemActions className="w-full sm:w-auto">
-            <Button
-              size="default"
-              variant="outline"
-              className="w-full sm:w-auto"
-              asChild
-            >
-              <Link href={ROUTES.LANGUAGES}>Languages</Link>
-            </Button>
-          </ItemActions>
-        </Item>
-      </div>
-    );
+    return <NoLanguagePairFallback />;
   }
 
   return <>{children}</>;
