ae8256a72d150e17d61e4e081a02ea932e3721d9
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-10-26 19:46:32 +0100

    Create fallback for no test settings

--- src/components/fallbacks/no-language-pair-fallback.tsx
+++ src/components/fallbacks/no-language-pair-fallback.tsx
@@ -1,43 +1,14 @@
 import { ROUTES } from '@/lib/constants/routes';
-import { ShieldAlert } from 'lucide-react';
-import Link from 'next/link';
-import { Button } from '../ui/button';
-import {
-  Item,
-  ItemActions,
-  ItemContent,
-  ItemDescription,
-  ItemMedia,
-  ItemTitle,
-} from '../ui/item';
+import { FallbackItem } from '../ui/fallback-item';
 
 export function NoLanguagePairFallback() {
   return (
-    <div className="grid justify-items-center items-start w-full h-full">
-      <Item variant="outline" className="flex-col sm:flex-row">
-        <ItemMedia
-          variant="icon"
-          className="!self-center sm:!self-start size-10 sm:size-8"
-        >
-          <ShieldAlert className="size-5 sm:size-4" />
-        </ItemMedia>
-        <ItemContent>
-          <ItemTitle>No language selected</ItemTitle>
-          <ItemDescription className="text-pretty">
-            Please select or create a language pair to continue.
-          </ItemDescription>
-        </ItemContent>
-        <ItemActions className="w-full sm:w-auto">
-          <Button
-            size="default"
-            variant="outline"
-            className="w-full sm:w-auto"
-            asChild
-          >
-            <Link href={ROUTES.LANGUAGES}>Languages</Link>
-          </Button>
-        </ItemActions>
-      </Item>
-    </div>
+    <FallbackItem
+      title="No language selected"
+      description="Please select or create a language pair to continue."
+      linkButton={true}
+      linkButtonHref={ROUTES.LANGUAGES}
+      linkButtonText="Languages"
+    />
   );
 }
