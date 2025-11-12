869ef0cfe58cd3e526e9065d1d52c6c0f2efacc2
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-11-12 21:45:09 +0100

    Turn off autoComplete in data-table filter input

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -86,6 +86,7 @@ export function DataTable<TData, TValue>({
               value={globalFilter}
               onChange={(e) => table.setGlobalFilter(e.target.value)}
               className="text-sm placeholder:italic"
+              autoComplete="off"
             />
             <InputGroupAddon>
               <Search />
